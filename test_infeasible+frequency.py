edc6a7bdc6ced5ca1028fbd44634ca066efce144
Mapl <yaphetsyyf2009@gmail.com>
2024-10-27 15:56:12 -0400

    Add new test files

---
+++ test_infeasible+frequency.py
@@ -0,0 +1,17 @@
+from bipartite_adjustment import Adjustment
+
+l = []
+w = []
+
+for t in [30,50,70]:
+    for i in [7,8,9]:
+        for j in [3,4,5]:
+            adj = Adjustment(trial=21, n_apps=10, T=t, links=range(30),
+                         max_n_states=20, max_n_flows=i, max_n_flow_hop=j,
+                         verbose=False)
+            results = adj.run()
+            
+            l.append(adj.infeasible_times)
+            w.append(len(results))
+
+print(l,w)
